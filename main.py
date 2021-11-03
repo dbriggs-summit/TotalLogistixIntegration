1dfe8855759ef06c2de74ad9202a94a460aaac70
dbriggs-summit <79608054+dbriggs-summit@users.noreply.github.com>
2021-11-03 11:16:02 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, exc
+import csv
+import logging
+from logging.config import dictConfig
+import config
+
+dictConfig(config.log_config)
+
+dyna_server = config.dynaServer
+dyna_db = config.dynaDBName
+dyna_user = config.dynaUserName
+dyna_password = config.dynaPassword
+dynacom_eng = \
+    create_engine(f'mssql+pyodbc://{dyna_user}:{dyna_password}@{dyna_server}/'
+                  f'{dyna_db}?driver=SQL Server')
+
+with dynacom_eng.connect() as con:
+    try:
+        result = con.execute('select * from view_TLOrders')
+        orders = result.fetchall()
+    except exc.SQLAlchemyError as e:
+        logging.error(e)
+
+
+with open(r'C:\Users\davidb.GARRISON\Downloads\TL Orders.csv', 'w', newline='') as csvfile:
+    try:
+        csvwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow(result.keys())
+        for line in orders:
+            csvwriter.writerow(line)
+    except FileError as e:
+        logging.error(e)
+logging.info('process completed successfully')
