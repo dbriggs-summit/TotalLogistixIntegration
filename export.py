18453669861bb7a92c3341cad5bba0e407ccf568
dbriggs-summit <79608054+dbriggs-summit@users.noreply.github.com>
2021-11-09 11:24:10 -0500

    Finalize SFTP support

--- export.py
+++ export.py
@@ -1,5 +1,6 @@
 from paramiko.client import SSHClient
 import config
+import os
 
 
 def push_orders(output_file):
@@ -9,21 +10,21 @@ def push_orders(output_file):
     password = config.ftpPassword
 
     client = SSHClient()
-    try:
-        host_keys = config.hostKeys
-    except KeyError:
-        host_keys = ''
+    client.load_system_host_keys()
 
     client.connect(hostname=host,
                    port=port,
                    username=username,
                    password=password,
-                   banner_timeout=30,
-                   key_filename=host_keys)
+                   banner_timeout=30)
 
     sftp = client.open_sftp()
 
-    path = './inbox/' + output_file
+    # Need to hardcode when accessing windows files from linux due to how os.path.basename works
+    # path = 'TL Orders.csv'
+    path = os.path.basename(output_file)
+    print(path)
+
     local_path = output_file
     sftp.put(local_path, path)
 
